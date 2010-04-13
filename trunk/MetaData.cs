b57c0bbb4e9b4e2768bb88424ced61572b07ce77
gg@splashstudio.com <gg@splashstudio.com@4287dfb2-2448-0410-a910-991614293e8c>
2010-04-13 17:19:59 +0000

    Added support for meta data, missing files (how embarrassing)

---
+++ trunk/MetaData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OSM2SHP {
+
+    /// <summary>
+    /// Holds meta data of shapes. Currently, the name is specified directly
+    /// and the rest of the OSM tags go into a dictionary that reflects the
+    /// key-value pairs used in OSM.
+    /// </summary>
+    public class MetaData
+    {
+        private string _name;
+        private Dictionary<string, string> _tags = new Dictionary<string,string>();
+
+        public string name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public Dictionary<string, string> tags
+        {
+            get { return _tags; }
+            set { _tags = value; }
+        }
+            
+    }
+}
